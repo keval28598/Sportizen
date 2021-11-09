037ff26efd5d9c9ae7e2333e14610c1330383589
Keval Patel <67217980+keval28598@users.noreply.github.com>
2021-11-09 00:50:11 -0500

    Revert "Images added"
    This reverts commit 9cfcd1576b7cb9b9cebf1f46fbed12a31b4a7d41.

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,8 +1,8 @@
 const products = [
   {
     _id: '1',
-    name: 'Baseball Ball',
-    image: '/public/images/baseball_ball.jpg',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
@@ -14,8 +14,8 @@ const products = [
   },
   {
     _id: '2',
-    name: 'Baseball Bat',
-    image: '/images/baseball_bat.jpg',
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
     description:
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     brand: 'Apple',
@@ -27,8 +27,8 @@ const products = [
   },
   {
     _id: '3',
-    name: 'Baseball Glove',
-    image: '/images/baseball_glove.jpg',
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
     description:
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
     brand: 'Cannon',
@@ -40,8 +40,8 @@ const products = [
   },
   {
     _id: '4',
-    name: 'Baseball Helmet',
-    image: '/images/baseball_helmet.jpg',
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     brand: 'Sony',
@@ -53,8 +53,8 @@ const products = [
   },
   {
     _id: '5',
-    name: 'Volleyball Ball',
-    image: '/images/vollyball_ball.jpg',
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
     description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     brand: 'Logitech',
@@ -66,8 +66,8 @@ const products = [
   },
   {
     _id: '6',
-    name: 'Volleyball Net',
-    image: '/images/vollyball_net.jpg',
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
     description:
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
     brand: 'Amazon',
