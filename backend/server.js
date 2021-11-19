72ece32102deea4ba57f7b943856faa23af4afae
Riya-Patel-2001 <69039370+Riya-Patel-2001@users.noreply.github.com>
2021-11-18 23:49:47 -0500

    User Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express  from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -12,22 +13,25 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
 
+import cors from "cors";
 
-import cors from 'cors';
+app.use(cors({origin: "* "}));
 
-app.use(cors({ origin: '* '}));
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
 
-app.get('/',(req,res) => {
-    res.send('Api is running')
-} )
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
-app.use('/api/products',productRoutes)
-
-app.use(notFound)
-app.use(errorHandler)
-
-
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
