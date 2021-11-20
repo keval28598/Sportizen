07ac933797aacff17b22e7244cc28e54a92500f5
Keval Patel <67217980+keval28598@users.noreply.github.com>
2021-11-19 19:01:26 -0500

    generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,14 @@
-import e from "express";
-import asyncHandler from "express-async-handler";
-import User from "../models/userModel.js";
+import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
+import User from '../models/userModel.js'
 
 // @desc    Auth user & get token
-// @route   POST /api/user/login
+// @route   POST /api/users/login
 // @access  Public
-
 const authUser = asyncHandler(async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body
 
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
     res.json({
@@ -17,12 +16,12 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
-    });
+      token: generateToken(user._id),
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
-});
+})
 
-export {authUser};
+export { authUser }
