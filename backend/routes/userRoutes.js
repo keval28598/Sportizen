0ad0b96fb0d9554257bb790169f3575d702132c7
Keval Patel <67217980+keval28598@users.noreply.github.com>
2021-11-21 23:48:02 -0500

    error correct

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,7 +12,7 @@ router.route('/').post(registerUser)
 router.post('/login', authUser)
 router
     .route('/profile')
-    .get(protect, getUserProfile, updateUser)
+    .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
 
 export default router
