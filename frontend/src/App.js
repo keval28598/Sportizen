479008a4e04cab7a4d68dec1a82fee7a0143b8e7
Keval Patel <67217980+keval28598@users.noreply.github.com>
2021-11-22 02:02:29 -0500

    order controller

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,9 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
-import ShippingScreen from './screens/ShippingScreen';
+import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
 
@@ -26,6 +27,7 @@ const App = () => {
       <Route path='/login' element={<LoginScreen/>} />
       <Route path='/shipping' element={<ShippingScreen/>} />
       <Route path='/payment' element={<PaymentScreen/>} />
+      <Route path='/placeorder' element={<PlaceOrderScreen/>} />
       <Route path='/register' element={<RegisterScreen/>} />
       <Route path='/profile' element={<ProfileScreen/>} />
       <Route path='/product/:id' element={<ProductScreen/>} />
