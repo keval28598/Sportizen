d6a9b5a181c8f0d7a587b0aed256fbcf0c12b069
Keval Patel <67217980+keval28598@users.noreply.github.com>
2021-11-22 04:46:42 -0500

    add check for order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const App = () => {
 
@@ -25,6 +26,7 @@ const App = () => {
       <Router>
       <Routes>
       <Route path='/login' element={<LoginScreen/>} />
+      <Route path='/order/:id' element={<OrderScreen/>} />
       <Route path='/shipping' element={<ShippingScreen/>} />
       <Route path='/payment' element={<PaymentScreen/>} />
       <Route path='/placeorder' element={<PlaceOrderScreen/>} />
