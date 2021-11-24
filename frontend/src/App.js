ac01693b86403f473151b3e84368300cad2c2d5e
Riya-Patel-2001 <69039370+Riya-Patel-2001@users.noreply.github.com>
2021-11-23 23:38:36 -0500

    Admin User List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
-import { Container } from 'react-bootstrap';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Container} from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen  from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen'
-import ProfileScreen from './screens/ProfileScreen'
-import ShippingScreen from './screens/ShippingScreen'
-import PaymentScreen from './screens/PaymentScreen'
-import PlaceOrderScreen from './screens/PlaceOrderScreen'
-import OrderScreen from './screens/OrderScreen'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 const App = () => {
-
   return (
-    
     <>
-    
       <Header />
       <main className="py-3">
-      <Container>
-      <Router>
-      <Routes>
-      <Route path='/login' element={<LoginScreen/>} />
-      <Route path='/order/:id' element={<OrderScreen/>} />
-      <Route path='/shipping' element={<ShippingScreen/>} />
-      <Route path='/payment' element={<PaymentScreen/>} />
-      <Route path='/placeorder' element={<PlaceOrderScreen/>} />
-      <Route path='/register' element={<RegisterScreen/>} />
-      <Route path='/profile' element={<ProfileScreen/>} />
-      <Route path='/product/:id' element={<ProductScreen/>} />
-      <Route path='/cart/:id' element={<CartScreen/>} />
-      <Route path='/' element={<HomeScreen/>} exact />
-
-      </Routes>
-      </Router>
-      </Container>
+        <Container>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<LoginScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />} />
+              <Route path="/shipping" element={<ShippingScreen />} />
+              <Route path="/payment" element={<PaymentScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/register" element={<RegisterScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+              <Route path="/cart/:id" element={<CartScreen />} />
+              <Route path="/admin/userlist" element={<UserListScreen />} />
+              <Route path="/" element={<HomeScreen />} exact />
+            </Routes>
+          </Router>
+        </Container>
       </main>
       <Footer />
-
-   </>
-    
-     
-     
-   
+    </>
   );
-}
+};
 
 export default App;
-
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,44 +1,62 @@
-import React from 'react';
-import { useDispatch,useSelector } from 'react-redux';
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
 // import {LinkContainer} from 'react-router-dom';
-import { Navbar,Nav,Container, NavDropdown} from 'react-bootstrap';
-import { logout } from '../actions/userActions';
+import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap";
+import {logout} from "../actions/userActions";
 
 const Header = () => {
-  const dispatch = useDispatch()
-  const userLogin = useSelector((state) => state.userLogin)
-  const {userInfo} = userLogin 
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const {userInfo} = userLogin;
 
   const logoutHandler = () => {
-    dispatch(logout())
-  }
-    return (
-        <header>
-<Navbar bg="" variend="light" expand="lg" collapseOnSelect >
-  <Container >
-    
-    <Navbar.Brand href='/' >Sportizen</Navbar.Brand>
-    
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav"  style={{ justifyContent: 'flex-end'}}>
-      <Nav className="mr-0">
-        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-        {userInfo ?(
-          <NavDropdown title={userInfo.name} id='username'>
-            <Nav.Link href="/profile"><i className="fas fa-user"></i>Profile</Nav.Link>
-            <NavDropdown.Item onClick={logoutHandler}>
-              Logout
-            </NavDropdown.Item>
-            </NavDropdown>
-        ) : (<Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>)}
-        
-       
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        </header>
-    )
-}
+    dispatch(logout());
+  };
+  return (
+    <header>
+      <Navbar bg="" variend="light" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Sportizen</Navbar.Brand>
 
-export default Header
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            style={{justifyContent: "flex-end"}}
+          >
+            <Nav className="mr-0">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <Nav.Link href="/profile">
+                    <i className="fas fa-user"></i>Profile
+                  </Nav.Link>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link href="/login">
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
+              )}
+
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <Nav.Link href="/admin/userlist">
+                    <i className="fas fa-user"></i>Users
+                  </Nav.Link>
+                  <Nav.Link href="/admin/productlist">Products</Nav.Link>
+                  <Nav.Link href="/admin/orderlist">Orders</Nav.Link>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
