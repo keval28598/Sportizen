8a25f7eaca3c609840f7895898f3681bd005493c
Jaskirat <jaskiratdhaliwal34@gmail.com>
2021-11-25 16:43:28 -0500

    Admin Create Product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,8 @@ import {Table, Button, Row, Col} from "react-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import {listProducts, deleteProduct} from "../actions/productActions";
+import {listProducts, deleteProduct, createProduct} from "../actions/productActions";
+import {PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_RESET} from '../constants/productConstants';
 
 const ProductListScreen = ({history, match}) => {
   const dispatch = useDispatch();
@@ -20,16 +21,29 @@ const ProductListScreen = ({history, match}) => {
     success: successDelete,
   } = productDelete;
 
+  const productCreate = useSelector((state) => state.productCreate);
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    product: createdProduct
+  } = productCreate;
+
   const userLogin = useSelector((state) => state.userLogin);
   const {userInfo} = userLogin;
 
   useEffect(() => {
-    if (userInfo && userInfo.isAdmin) {
-      dispatch(listProducts());
-    } else {
+    dispatch ({ type: PRODUCT_CREATE_RESET})
+    if (!userInfo.isAdmin) {
       navigate("/login");
+    } 
+    if (successCreate){
+      navigate(`admin/product/${createdProduct._id}/edit`)
     }
-  }, [dispatch, userInfo, successDelete]);
+    else{
+      dispatch(listProducts())
+    }
+  }, [dispatch, userInfo, successDelete, successCreate, createdProduct]);
 
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure")) {
@@ -37,8 +51,8 @@ const ProductListScreen = ({history, match}) => {
     }
   };
 
-  const createProductHandler = (product) => {
-    //   CREATE PRODUCT
+  const createProductHandler = () => {
+    dispatch(createProduct())
   };
 
   return (
@@ -55,6 +69,8 @@ const ProductListScreen = ({history, match}) => {
       </Row>
       {loadingDelete && <Loader />}
       {errorDelete && <Message variant="danger">{errorDelete}</Message>}
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant="danger">{errorCreate}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
