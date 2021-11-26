1f4429a6dcc2bcf2a3c98e222ecb9d6631388f27
Riya-Patel-2001 <69039370+Riya-Patel-2001@users.noreply.github.com>
2021-11-25 23:28:07 -0500

    Admin Order List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,9 +14,10 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
-import UserEditScreen from './screens/UserEditScreen';
-import ProductListScreen from './screens/ProductListScreen';
+import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
 
 const App = () => {
   return (
@@ -33,12 +34,19 @@ const App = () => {
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/register" element={<RegisterScreen />} />
               <Route path="/profile" element={<ProfileScreen />} />
-              <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
-              <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
-              <Route path='/admin/productlist' element={<ProductListScreen />} />
+              <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+              <Route
+                path="/admin/product/:id/edit"
+                element={<ProductEditScreen />}
+              />
+              <Route
+                path="/admin/productlist"
+                element={<ProductListScreen />}
+              />
               <Route path="/product/:id" element={<ProductScreen />} />
               <Route path="/cart/:id" element={<CartScreen />} />
               <Route path="/admin/userlist" element={<UserListScreen />} />
+              <Route path="/admin/orderlist" element={<OrderListScreen />} />
               <Route path="/" element={<HomeScreen />} exact />
             </Routes>
           </Router>
